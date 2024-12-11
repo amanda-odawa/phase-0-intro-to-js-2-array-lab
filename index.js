d123ad67e0ceb0a1eb71846ae51a019dc2b3d226
AmandaOdawa <amandaodawa1@gmail.com>
2024-12-11 10:33:00 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name = "Ralph") {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name = "Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name){
+    const newCats = cats.slice();
+    newCats.push(name = "Broom");
+    return newCats;
+}
+
+function prependCat(name = "Arnold") {
+    const newCats = [...cats];
+    newCats.unshift(`${name}`);
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCats = cats.slice();
+    newCats.pop();
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = [...cats];
+    newCats.shift();
+    return newCats;
+}
